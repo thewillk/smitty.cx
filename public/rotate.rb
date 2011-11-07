29233702a0f16dddbfa7c4a8bf52b05f5d05382f
Will Klancnik <william@klancnik.com>
2011-11-07 16:40:43 -0500

    Updated rotation script to use smitty library

--- public/rotate.rb
+++ public/rotate.rb
@@ -1,9 +1,9 @@
 #!/usr/bin/ruby
 
-themes = ["smitty-blue","smitty-red","smitty-green","smitty-white","smitty-aqua","smitty-orange","smitty-yellow","smitty-purple"]
+require '~/.smitty/smitty.rb'
 
-# NEED TO UPDATE TO ACCOMODATE TABS
+themes = ["smitty-yellow","smitty-red","smitty-blue","smitty-green","smitty-white","smitty-aqua","smitty-orange","smitty-purple"]
+terminal = Smitty::Terminal.new
 
-n = `osascript -e 'tell application "Terminal" to get the index of the last window'`
-`osascript -e 'tell application "Terminal" to set current settings of front window to first settings set whose name is "#{themes[(Integer(n)-1)%themes.length]}"'`
-
+n = terminal.number_of_open_tabs
+terminal.set_current_window_to_theme themes[(Integer(n)-1)%themes.length]
